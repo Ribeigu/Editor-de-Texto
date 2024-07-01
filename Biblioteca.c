d19585e5f3c988209cec188e3e8bd76d15455089
Ribeigu <gustavo.r.mota06@gmail.com>
2024-07-01 14:25:53 -0300

    quase la

--- Biblioteca.c
+++ Biblioteca.c
@@ -55,3 +55,74 @@ void deleteCaractere(PILHA *p, int caso){
     p->topo->prox=NULL;
     free(aux);
 }
+void criarArquivo() {
+    char nome[20], c;//VAR PARA O NOME DO ARQUIVO E PARA OS CARACTERES
+    FILE *file1, *file2;//VAR PARA OS ARQUIVOS
+    file1 = fopen("temp.txt", "w");//INICIALIZA O ARQUIVO TEMPOR�RIO
+    if (file1 == NULL) {
+        printf("\nERRO AO ABRIR ARQUIVO");//ABRE UM ARQUIVO TEMPOR�RIO CASO N�O D� ERRO
+        return;
+    }
+
+    printf("\nINSIRA O TEXTO E APERTE '.' PARA SALVAR\n");
+    while ((c = getchar()) != '.') {
+        fputc(c, file1);//L� O CARACTER E SALVA NO ARQUIVO
+    }
+    fputc(c, file1);//SALVA O PONTO NO ARQUIVO
+    fclose(file1);//FECHA O ARQUIVO TEMPOR�RIO
+
+    printf("\nINSIRA O NOME DO ARQUIVO: ");
+    scanf("%s", nome);
+    fflush(stdin);//L� O NOME E LIMPA O BUFFER
+
+    file1 = fopen("temp.txt", "r");
+    file2 = fopen(nome, "w");//ABRE OS DOIS ARQUIVOS
+    if (file1 == NULL || file2 == NULL) {
+        printf("\nERRO AO ABRIR ARQUIVO");//PRINTA ERRO CASO D� ERRO
+        return;
+    }
+
+    while ((c = getc(file1)) != EOF) {
+        putc(c, file2);//PASSA O QUE FOI ESCRITO DO ARQUIVO TEMPOR�RIO PARA O FINAL
+    }
+    fclose(file1);
+    fclose(file2);
+    remove("temp.txt");//FECHA OS ARQUIVOS E APAGA O TEMPOR�RIO
+}
+
+void mostrarArquivo() {
+    char nome[20], c;//VARI�VEIS DO ARQUIVO
+    FILE *file1;//INICIALIZA O ARQUIVO
+    printf("\nINSIRA O NOME DO ARQUIVO: ");
+    scanf("%s", nome);//NOME DO ARQUIVO QUE SER� BUSCADO
+    fflush(stdin);//LIMPA O BUFFER
+
+    file1 = fopen(nome, "r");//ABRE O ARQUIVO
+    if (file1 == NULL) {
+        printf("\nERRO AO BUSCAR ARQUIVO");//ERRO SE N�O ACHAR O ARQUIVO
+    } else {
+        while ((c = getc(file1)) != EOF) {
+            printf("%c", c);//PRINTA O CONTE�DO DO ARQUIVO
+        }
+        fclose(file1);//FECHA O ARQUIVO
+    }
+
+    printf("\nAPERTE QUALQUER TECLA PARA CONTINUAR");
+    getch();
+}
+
+void apagarArquivo() {
+    char nome[20];
+    printf("\nINSIRA O NOME DO ARQUIVO: ");
+    scanf("%s", nome);
+    fflush(stdin);//LIMPA O BUFFER
+
+    if (remove(nome) == 0) {
+        printf("\nARQUIVO DELETADO");
+    } else {
+        printf("\nERRO AO DELETAR ARQUIVO");
+    }
+
+    printf("\nAPERTE QUALQUER TECLA PARA CONTINUAR");
+    getch();
+}
--- Principal.c
+++ Principal.c
@@ -3,77 +3,6 @@
 #include <stdlib.h>//BIBLIOTECAS USADAS
 #include <conio.h>
 
-void criarArquivo() {
-    char nome[20], c;//VAR PARA O NOME DO ARQUIVO E PARA OS CARACTERES
-    FILE *file1, *file2;//VAR PARA OS ARQUIVOS
-    file1 = fopen("temp.txt", "w");//INICIALIZA O ARQUIVO TEMPORÁRIO
-    if (file1 == NULL) {
-        printf("\nERRO AO ABRIR ARQUIVO");//ABRE UM ARQUIVO TEMPORÁRIO CASO NÃO DÊ ERRO
-        return;
-    }
-
-    printf("\nINSIRA O TEXTO E APERTE '.' PARA SALVAR\n");
-    while ((c = getchar()) != '.') {
-        fputc(c, file1);//LÊ O CARACTER E SALVA NO ARQUIVO
-    }
-    fputc(c, file1);//SALVA O PONTO NO ARQUIVO
-    fclose(file1);//FECHA O ARQUIVO TEMPORÁRIO
-
-    printf("\nINSIRA O NOME DO ARQUIVO: ");
-    scanf("%s", nome);
-    fflush(stdin);//LÊ O NOME E LIMPA O BUFFER
-
-    file1 = fopen("temp.txt", "r");
-    file2 = fopen(nome, "w");//ABRE OS DOIS ARQUIVOS
-    if (file1 == NULL || file2 == NULL) {
-        printf("\nERRO AO ABRIR ARQUIVO");//PRINTA ERRO CASO DÊ ERRO
-        return;
-    }
-
-    while ((c = getc(file1)) != EOF) {
-        putc(c, file2);//PASSA O QUE FOI ESCRITO DO ARQUIVO TEMPORÁRIO PARA O FINAL
-    }
-    fclose(file1);
-    fclose(file2);
-    remove("temp.txt");//FECHA OS ARQUIVOS E APAGA O TEMPORÁRIO
-}
-
-void mostrarArquivo() {
-    char nome[20], c;//VARIÁVEIS DO ARQUIVO
-    FILE *file1;//INICIALIZA O ARQUIVO
-    printf("\nINSIRA O NOME DO ARQUIVO: ");
-    scanf("%s", nome);//NOME DO ARQUIVO QUE SERÁ BUSCADO
-    fflush(stdin);//LIMPA O BUFFER
-
-    file1 = fopen(nome, "r");//ABRE O ARQUIVO
-    if (file1 == NULL) {
-        printf("\nERRO AO BUSCAR ARQUIVO");//ERRO SE NÃO ACHAR O ARQUIVO
-    } else {
-        while ((c = getc(file1)) != EOF) {
-            printf("%c", c);//PRINTA O CONTEÚDO DO ARQUIVO
-        }
-        fclose(file1);//FECHA O ARQUIVO
-    }
-
-    printf("\nAPERTE QUALQUER TECLA PARA CONTINUAR");
-    getch();
-}
-
-void apagarArquivo() {
-    char nome[20];
-    printf("\nINSIRA O NOME DO ARQUIVO: ");
-    scanf("%s", nome);
-    fflush(stdin);//LIMPA O BUFFER
-
-    if (remove(nome) == 0) {
-        printf("\nARQUIVO DELETADO");
-    } else {
-        printf("\nERRO AO DELETAR ARQUIVO");
-    }
-
-    printf("\nAPERTE QUALQUER TECLA PARA CONTINUAR");
-    getch();
-}
 
 void adicionarTexto() {
     char nome[20], c;
@@ -89,10 +18,10 @@ void adicionarTexto() {
         printf("\nINSIRA O TEXTO E APERTE 'CTRL+S' PARA SALVAR\n");
         while (1) {
             c = getch();
-            if (c == 19) {//19 É O CÓDIGO ASCII PARA CTRL+S
+            if (c == 19) {//19 ï¿½ O Cï¿½DIGO ASCII PARA CTRL+S
                 break;
             }
-            if (c == 13) {//13 É O CÓDIGO ASCII PARA ENTER
+            if (c == 13) {//13 ï¿½ O Cï¿½DIGO ASCII PARA ENTER
                 c = '\n';
                 printf("\n");
             } else {
@@ -111,7 +40,7 @@ int main() {
     int opcao;
     do {
         system("cls");//LIMPA A TELA
-        printf("\n-//-//-EDITOR DE TEXTO-//-//-");//MENU COM AS OPÇÕES
+        printf("\n-//-//-EDITOR DE TEXTO-//-//-");//MENU COM AS OPï¿½ï¿½ES
         printf("\n-//-MENU-//-");
         printf("\n[1] CRIAR NOVO ARQUIVO\n[2] MOSTRAR ARQUIVO\n[3] ADICIONAR TEXTO\n[4] APAGAR ARQUIVO\n[5] SAIR\n");
         printf("\nINSIRA O QUE FAZER: ");
