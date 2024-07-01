0bff3504a77dd3b802847dd0d4025b03b41a770e
Ribeigu <gustavo.r.mota06@gmail.com>
2024-07-01 11:34:03 -0300

    nem sei mais

--- Biblioteca.c
+++ Biblioteca.c
@@ -21,37 +21,61 @@ int pilhaVazia(PILHA *p){
     if(p->topo==NULL) return TRUE;
     return FALSE;
 }
-void lerCaractere(PILHA *p){
+void lerCaractere(PILHA *p[], int pos){
     NO *no=criarNO();
     no->letra=getchar();
-    if(pilhaVazia(p)){
-        p->topo=no;
+    if(pilhaVazia(p[pos])){
+        p[pos]->topo=no;
     }
     switch(no->letra){
-        case 8:
-        deleteCaractere(p, 8);
+        case 8: //backspace
+        deleteCaractere(p, 8, pos);
             break;
-        case 127:
-            deleteCaracter(p,9);
+        case 127: //delete
+            deleteCaracter(p,9,pos);
             break;
         
     }
     no->ant=p;
-    p->topo->prox=no;
-    p->topo=no;
+    p[pos]->topo->prox=no;
+    p[pos]->topo=no;
 }
-void deleteCaractere(PILHA *p, int caso){
-    if(!pilhaVazia(p)){
+void deleteCaractere(PILHA *p[], int caso, int pos){
+    if(!pilhaVazia(p[pos])){
         //fazer voltar pra linha anterior. Exclui a linha
         if(caso==8){
-
+            if(p[pos-1]->topo!=NULL){
+                free(p[pos]);
+                return p[pos-1]->topo;
+            }
         }
         if(caso==9){
-
+            if(p[pos+1]->topo!=NULL){
+                free(p[pos]);
+                return p[pos+1]->topo;
+            }
         }
     }
-    NO *aux=p->topo;
-    p->topo= aux->ant;
-    p->topo->prox=NULL;
+    NO *aux=p[pos]->topo;
+    p[pos]->topo= aux->ant;
+    p[pos]->topo->prox=NULL;
     free(aux);
+    return p[pos];
+}
+
+void navegar(PILHA *p[],int direcao, int pos){
+    switch(direcao){
+        case esquerda:
+        if(p[pos]->topo->prox!=NULL) return p[pos]->topo->prox;
+        if(p[pos+1]->topo!=NULL) return p[pos+1]->topo;
+            break;
+        case direita:
+            if(p[pos]->topo->ant!=NULL) return p[pos]->topo->ant;
+            if(p[pos-1]->topo!=NULL) return p[pos-1]->topo;
+            break;
+        case cima:
+            break;
+        case baixo;
+            break;
+    }
 }
